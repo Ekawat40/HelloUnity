88a2d5207d71a97331053f3b66100bc50d643207
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 00:52:01 +0700

    Intro Script and Variable

---
+++ Assets/Script/Variable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour {
+
+    int number = 10; //เก็บจำนวนเต็ม
+    bool status = true;
+    string str = "Ekawat";
+    double db = 2540.25;
+    float f = 15.5f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Result: " + number);
+        print("Status: " + status);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
