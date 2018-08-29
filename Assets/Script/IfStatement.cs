63fe1c6c39815956efa5cac80dbbdd57f6b488f5
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 01:09:15 +0700

    If Statement and Function

---
+++ Assets/Script/IfStatement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatement : MonoBehaviour {
+
+    public int number = 10;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            addFunction();
+        }
+	}
+
+    void addFunction()
+    {
+        /* if (number == 10)
+         {
+             print("Number = " + 10);
+         } else if (number>=10)
+         {
+             print("OK");
+         }
+         else
+         {
+             print("Not Found");
+         }*/
+
+        if (number == 10)
+        {
+            print("Number = " + 10);
+        }
+        if (number >= 10)
+        {
+            print("OK");
+        }
+    }
+}
