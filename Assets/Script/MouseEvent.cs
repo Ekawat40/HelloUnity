d8796749db827c2c0b8433cb5e5a2d4f1393fbaa
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 20:39:22 +0700

    OnMouseDown

---
+++ Assets/Script/MouseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvent : MonoBehaviour {
+
+    public float forceSpeed = 300;
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(-transform.forward * forceSpeed);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
