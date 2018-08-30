c0539ae8d0e830451c7c400f0f1b2b0e38efffd6
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 19:33:22 +0700

    DoWhile and For Loop

---
+++ Assets/Script/DoWhileLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour {
+
+    bool status = false;
+	// Use this for initialization
+	void Start () {
+        do
+        {
+            Debug.Log("Ekawat");
+        } while (status == true);
+        {
+
+        }
+
+	}
+	
+}
---
+++ Assets/Script/ForLoop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour {
+    bool status = false;
+	// Use this for initialization
+	void Start () {
+        int i;
+        for( i = 0; i < 10; i++)
+        {
+            print("i= " + i);
+        }
+	}
+
+}
