65211f8c8cbf1802ce5828f547c4cdece67812d5
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 19:19:18 +0700

    WhileLoop

---
+++ Assets/Script/WhileLoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour {
+
+    int count = 10;
+
+	// Use this for initialization
+	void Start () {
+        while (count > 0)
+        {
+            Debug.Log("Ekawat");
+            count--;
+        }
+	}
+	
+
+}
