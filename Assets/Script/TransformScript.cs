adf039636fda92b343228d8d771b7c3327ca8600
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 19:48:11 +0700

    Transform and Time deltaTime

---
+++ Assets/Script/TransformScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformScript : MonoBehaviour {
+
+
+    public int speed=2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // transform.Translate(new Vector3(0, 0, 1)*Time.deltaTime);
+        transform.Translate(Vector3.forward*speed* Time.deltaTime);
+
+
+    }
+}
