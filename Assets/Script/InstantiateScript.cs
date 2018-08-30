90d7113b7e520523b57cac29f0a366c118d2d353
Ekawat40 <aekwatt@hotmail.com>
2018-08-30 21:15:49 +0700

    cloning Object with Instantiate

---
+++ Assets/Script/InstantiateScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateScript : MonoBehaviour {
+
+    public Rigidbody prefab;
+    public Transform target;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(prefab,target.position,target.rotation);
+        }
+	}
+}
